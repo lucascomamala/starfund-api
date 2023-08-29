7a4af26ea2c01b0780d36aa01c45f6666b47d16a
Lucas <lucascomamala@gmail.com>
2023-08-29 02:26:29 -0600

    install morgan

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World2!');
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+app.use(morgan('dev'))
+
+app.listen(3001)
+console.log('Server on port', 3001)
